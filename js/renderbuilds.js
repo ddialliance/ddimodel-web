6d217741b92c31a316968e8bb4a65ba879fbf503
Dan Smith <dan@colectica.com>
2017-10-12 01:21:53 -0500

    Improved build rendering javascript

--- js/renderbuilds.js
+++ js/renderbuilds.js
@@ -1,16 +1,29 @@
 function renderCards(data) {
     
     var html = '';
+    var count = 0;
     $.each(data.builds, function(key, value) {
   
         console.log("Adding Card");
+
+        if(count == 0){
+            document.getElementById("latestCommitLink").href=value.APPVEYOR_REPO_COMMIT; 
+            count++;
+        }
+
         html += '<li class="collection-item avatar">';
-        html += '<img src="https://github.com/' + value.APPVEYOR_REPO_COMMIT_AUTHOR + '.png" alt="" class="circle">';
-        html += '<span class="title">Title</span>';
-        html += '<p>First Line <br>';
-        html += '   Second Line';
+        html += '<a href="' + value.github_commit_info.html_url + '">';
+        html += '<img src="' + value.github_commit_info.avatar_url + '" alt="'+value.APPVEYOR_REPO_COMMIT_AUTHOR+'" class="circle"></a>';
+        html += '<span class="title"><a href="' + value.APPVEYOR_REPO_COMMIT + '">DDI Docs '+value.APPVEYOR_REPO_COMMIT_TIMESTAMP+'</a></span>';
+        html += '<p>'+ value.APPVEYOR_REPO_COMMIT_MESSAGE +'</br>';
+        html += value.APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED;
+
+        html += 'Commit <a href="https://github.com/ddialliance/ddimodel/commit/' + value.APPVEYOR_REPO_COMMIT + '">' + value.APPVEYOR_REPO_COMMIT.substring(0,7) + '...</a></br>';
+        html += 'View <a href="' + value.APPVEYOR_REPO_COMMIT + '">Documentation</a></br>';
+        html += 'Download <a href="' + value.APPVEYOR_REPO_COMMIT + '/_static/ddi.zip">All Outputs</a>';
+        
         html += '</p>';
-        html += '<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>';
+        html += '<a href="' + value.APPVEYOR_REPO_COMMIT + '/_static/ddi.zip" class="secondary-content"><i class="medium material-icons">file_download</i></a>';
         html += '</li>';
 
         html += '</div>';
